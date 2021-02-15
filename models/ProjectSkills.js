a381c8103cd1a28417a2277613432186cb5be485
Coleman Buffa <coleman.buffa@gmail.com>
2021-02-15 14:58:30 -0800

    Draft schema for ProjectSkills table.

--- models/ProjectSkills.js
+++ models/ProjectSkills.js
@@ -1,5 +1,8 @@
 module.exports = function (sequelize, DataTypes) {
 	let ProjectSkills = sequelize.define("ProjectSkills", {		
 	});
-	//Define associations between tables here
+	ProjectSkills.associate = models => {
+		models.ProjectSkills.belongsTo(models.Project, {onDelete: "cascade"});
+		models.ProjectSkills.belongsTo(models.Skill, {onDelete: "cascade"});
+	}
 };
