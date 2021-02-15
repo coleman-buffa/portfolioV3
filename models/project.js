8afb80ab320175c3a26e279424fa03c9c7510d22
Coleman Buffa <coleman.buffa@gmail.com>
2021-02-15 14:45:30 -0800

    Draft schema for Project table.

--- models/project.js
+++ models/project.js
@@ -1,7 +1,19 @@
 module.exports = function (sequelize, DataTypes) {
 	let Project = sequelize.define("Project", {		
-		//Define schema for Project here
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		link: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
 	});
-	//Define associations between tables here
-
+	Project.associate = models => {
+		models.Project.hasMany(models.ProjectSkills, {onDelete: "cascade"});
+	}
 };
