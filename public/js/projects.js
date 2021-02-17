51049a38277b9041d6eb6048149c6aeccc85044a
Coleman Buffa <coleman.buffa@gmail.com>
2021-02-16 19:21:08 -0800

    Render project list to navigation dropdown menu.

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   $.get("/api/allprojects").then((response) => {
-    console.log(response);
+    renderProjectList(response);
   });
-  
+
 });
+
+function renderProjectList(input) {
+  input.forEach(element => {
+    let html = `<li><a href=#>${element.title}</a><li>`;
+    $("#projectList").append(html);
+  });
+}
